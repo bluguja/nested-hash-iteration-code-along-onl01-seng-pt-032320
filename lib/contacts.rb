3d98f94f731b84352004d0a7757e3b34c7c308e8
Bala Luguja <bluguja@gmail.com>
2020-04-05 01:54:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,9 +15,12 @@
    }
   
 def remove_strawberry(contacts)
-puts contacts ["Jon Snow"] [:email]
-puts "//////"
-puts contacts ["Jon Snow"].keys
-puts contacts ["Jon Snow"].values
+contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+     end 
+    end
+  end
 
 end
